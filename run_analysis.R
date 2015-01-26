8070ce7e3ac05372e789234fa36be68fcf4b14de
rafalpw <rafal.powalski@gmail>
2015-01-26 01:26:16 +0100

    r code

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 #load csv files
-test<-read.table("getdata/test/X_test.txt")
-train<-read.table("getdata/train/X_train.txt")
-sbjTest<- read.table("getdata/test/subject_test.txt")
-sbjTrain<- read.table("getdata/train/subject_train.txt")
-lblTest<- read.table("getdata/test/y_test.txt")
-lblTrain<- read.table("getdata/train/y_train.txt")
-features <- read.table("getdata/features.txt")
-labels <- read.table("getdata/activity_labels.txt")
+test<-read.table("test/X_test.txt")
+train<-read.table("train/X_train.txt")
+sbjTest<- read.table("test/subject_test.txt")
+sbjTrain<- read.table("train/subject_train.txt")
+lblTest<- read.table("test/y_test.txt")
+lblTrain<- read.table("train/y_train.txt")
+features <- read.table("features.txt")
+labels <- read.table("activity_labels.txt")
 
 #merge datasets
 all<- rbind(cbind(sbjTest, lblTest, test),cbind(sbjTrain, lblTrain, train))
@@ -20,7 +20,7 @@ small<- all[ ,grep('subjectID|activity|mean\\(|std\\(', a)]
 #data set with the average of each variable for each activity and each subject
 smry_set<-aggregate(small[,3:68], list(subjectID=small$subjectID, activity=small$activity), mean)
 #save result table
-write.table(smry_set, "getdata/smry_set.txt", row.name=F)
+write.table(smry_set, "smry_set.txt", row.name=F)
 
 
 
